d39a0fe772ffd69efa89f2f7eca7cb6d445fe190
dawsonblock <dawsonblock@users.noreply.github.com>
2025-12-07 01:09:02 -0600

    Fix: Update __init__.py version to 5.0.0

--- cyborg_rl/__init__.py
+++ cyborg_rl/__init__.py
@@ -5,6 +5,6 @@ from cyborg_rl.utils.device import get_device
 from cyborg_rl.utils.logging import get_logger
 from cyborg_rl.utils.seeding import set_seed
 
-__version__ = "2.8.0"
+__version__ = "5.0.0"
 
 __all__ = ["Config", "get_device", "get_logger", "set_seed"]
